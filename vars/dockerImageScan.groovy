f62c9a5716494935cdbe40ee4122f5b77fd30254
jerry44000 <shai.badarna@gmail.com>
2023-06-13 19:37:41 +0200

    docker scan

---
+++ vars/dockerImageScan.groovy
@@ -0,0 +1,8 @@
+def call(String project,String ImageTag, String hubUser){
+
+    sh """
+
+      trivy image ${hubUser}/${project} ${hubUser}/${project}:latest > scan.txt
+      cat scan.txt
+    """
+}
