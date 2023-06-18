c1fcd20e699f47f6dd27a2b175fe1228cf2bad39
jerry44000 <shai.badarna@gmail.com>
2023-06-18 12:16:09 +0200

    ECR shared library rectif cleanup

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,7 +1,7 @@
 def call(String project,String ImageTag, String hubUser){
-
+    
     sh """
-      docker rmi ${hubUser}/${project}:${ImageTag}
-      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+     docker rmi ${hubUser}/${project}:${ImageTag}
+     docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
     """
 }
